1b5c3a2fabbae166466a4d69a17c03aa4aeec3db
Jose Manuel Sanchez <99229719+DaddyLipi@users.noreply.github.com>
2022-06-06 19:35:29 -0400

    Test anio 3, rojo

--- src/biciesto.js
+++ src/biciesto.js
@@ -1,12 +1,20 @@
-function TestBiciesto(anio)
+/*function TestBiciesto(anio)
 {
-    if(anio==1)
-    {
-        return 1
-    }
-    else
+    if(anio==0)
     {
         return 0;
     }
+    else
+    {
+        if(anio%4==0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
 }
 export default TestBiciesto;
+*/
--- src/biciesto.test.js
+++ src/biciesto.test.js
@@ -9,4 +9,7 @@ describe("TDD", () => {
     it("Ingresar anio 4", () => {
         expect(TestBiciesto(4)).toBe(0);
     });
+    it("Ingresar anio 3", () => {
+        expect(TestBiciesto(3)).toBe(1);
+    });
 }); 
