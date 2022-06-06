ba8a3e6f381e938795cbb335f0bbba47a65deb9e
Jose Manuel Sanchez <99229719+DaddyLipi@users.noreply.github.com>
2022-06-06 18:44:35 -0400

    Test anio 1, verde

--- src/biciesto.test.js
+++ src/biciesto.test.js
@@ -3,4 +3,7 @@ describe("TDD", () => {
     it("Ingresar anio 0", () => {
     expect(TestBiciesto(0)).toBe(0);
     });
+    it("Ingresar anio 1", () => {
+    expect(TestBiciesto(1)).toBe(1);
+    });
 }); 
